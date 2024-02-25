daebbdbf5e75dd1f555293471b9e93cd41c4bf3b
Jhon-mantila <kaiser_4tu@hotmail.com>
2024-02-25 18:33:54 -0500

    Collection en api

--- app/Http/Controllers/Api/v1/PostController.php
+++ app/Http/Controllers/Api/v1/PostController.php
@@ -15,6 +15,8 @@ class PostController extends Controller
     public function index()
     {
         //
+        return PostResource::collection(Post::latest()->paginate());
+        //return Post::latest()->paginate();
     }
 
     /**
