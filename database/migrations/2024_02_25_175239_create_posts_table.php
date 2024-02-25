43232e74950cfc6057b110358fdb471bdad2d443
Jhon-mantila <kaiser_4tu@hotmail.com>
2024-02-25 13:50:39 -0500

    Video 3, Creando base de datos, campos de la tabla post, datos feke de la tabla post, controlador del post

--- database/migrations/2024_02_25_175239_create_posts_table.php
+++ database/migrations/2024_02_25_175239_create_posts_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
             $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [\App\Http\Controllers\PostController::class, 'index']);
+
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
