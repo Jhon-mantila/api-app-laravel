e35526f4e65d656c4d3e9ac944a3ca49e065ae36
Jhon-mantila <kaiser_4tu@hotmail.com>
2024-02-26 21:22:30 -0500

    Creando el json de retorno para version 2 y la realcion con usuarios para retornar

--- app/Http/Resources/v2/PostResource.php
+++ app/Http/Resources/v2/PostResource.php
@@ -14,6 +14,17 @@ class PostResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'post_name' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+            'autor' => [
+                'name'=> $this->user->name,
+                'email'=> $this->user->email,
+            ],
+            'create_at' => $this->published_at
+        ];
     }
 }
