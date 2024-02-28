8d1fba06b503aa175e452887e98d5429f1bd713f
Jhon-mantila <kaiser_4tu@hotmail.com>
2024-02-27 19:16:22 -0500

    Comenzao para crear un token(login)

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\v1\PostController as PostV1;
 use App\Http\Controllers\Api\v2\PostController as PostV2;
-
+use App\Http\Controllers\Api\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +22,10 @@ use App\Http\Controllers\Api\v2\PostController as PostV2;
 });*/
 
 Route::apiResource('v1/posts', PostV1::class);
-Route::apiResource('v2/posts', PostV2::class)->only(['index', 'show']);
+//v2
+Route::apiResource('v2/posts', PostV2::class)
+        ->only(['index', 'show'])
+        ->middleware('auth:sanctum');
+
+Route::post('login', [App\Http\Controllers\Api\LoginController::class, 'login']);
 
